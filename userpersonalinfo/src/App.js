e1c7ab72f05dbf9741ca52db560829d516c21ba6
SamLawson1110 <sammaker1110@gmail.com>
2022-07-18 14:33:46 +0000

    first commit

---
+++ userpersonalinfo/src/App.js
@@ -0,0 +1,12 @@
+import UserInfo from './components/UserInfo';
+import styles from './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <UserInfo></UserInfo>
+    </div>
+  );
+}
+
+export default App;
